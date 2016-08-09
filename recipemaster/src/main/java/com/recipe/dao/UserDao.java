0082b937c971334920d497854c8369158900b367
S2OL <S2OL@192.168.0.50>
2016-08-09 20:15:16 +0900

    UserDao.java 수정 - 박상

--- recipemaster/src/main/java/com/recipe/dao/UserDao.java
+++ recipemaster/src/main/java/com/recipe/dao/UserDao.java
@@ -6,9 +6,12 @@ import java.util.Map;
 import com.recipe.domain.User;
 
 public interface UserDao {
-  void insert(User user);
+  Map insert(User user);
   List<User> selectList(Map<String,Object> params);
   User selectOne(int no);
+  User checkDuplication(String email);
+  User checkDuplicationUserName(String userName);
   int update(User user);
   int delete(int no);
+  User findUser(String email);//Read or Retrieve
 }
