73fc05bfbe07544b517aac42b962462c36587521
S2OL <S2OL@localhost>
2016-08-10 16:32:00 +0900

    레시피 쓰기 페이지 수정 및 재료 등록 ok - 박상일

--- recipemaster/src/main/webapp/js/writepage.js
+++ recipemaster/src/main/webapp/js/writepage.js
@@ -1,163 +1,163 @@
-	/*
+/*
 	 jQuery Image upload
 	 Images can be uploaded using:
-	 * File requester (Double click on box)
-	 * Drag&Drop (Drag and drop image on box)
-	 * Pasting. (Copy an image or make a screenshot, then activate the page and paste in the image.)
+ * File requester (Double click on box)
+ * Drag&Drop (Drag and drop image on box)
+ * Pasting. (Copy an image or make a screenshot, then activate the page and paste in the image.)
 
 	 Works in Mozilla, Webkit & IE.
-	 */
+ */
 
-	jQuery(document)
-			.ready(
-					function($) {
+jQuery(document)
+		.ready(
+				function($) {
 
-						// Shared callback handler for processing output
-						var outputHandlerFunc = function(imgObj) {
+					// Shared callback handler for processing output
+					var outputHandlerFunc = function(imgObj) {
 
-							var sizeInKB = function(bytes) {
-								return (typeof bytes == 'number') ? (bytes / 1024)
-										.toFixed(2)
-										+ 'Kb'
-										: bytes;
-							};
+						var sizeInKB = function(bytes) {
+							return (typeof bytes == 'number') ? (bytes / 1024)
+									.toFixed(2)
+									+ 'Kb' : bytes;
+						};
 
-							var getThumbnail = function(original, maxWidth,
-									maxHeight, upscale) {
-								var canvas = document.createElement("canvas"), width, height;
-								if (original.width < maxWidth
-										&& original.height < maxHeight
-										&& upscale == undefined) {
-									width = original.width;
-									height = original.height;
-								} else {
-									width = maxWidth;
-									height = parseInt(original.height
-											* (maxWidth / original.width));
-									if (height > maxHeight) {
-										height = maxHeight;
-										width = parseInt(original.width
-												* (maxHeight / original.height));
-									}
+						var getThumbnail = function(original, maxWidth,
+								maxHeight, upscale) {
+							var canvas = document.createElement("canvas"), width, height;
+							if (original.width < maxWidth
+									&& original.height < maxHeight
+									&& upscale == undefined) {
+								width = original.width;
+								height = original.height;
+							} else {
+								width = maxWidth;
+								height = parseInt(original.height
+										* (maxWidth / original.width));
+								if (height > maxHeight) {
+									height = maxHeight;
+									width = parseInt(original.width
+											* (maxHeight / original.height));
 								}
-								canvas.width = width;
-								canvas.height = height;
-								canvas.getContext("2d").drawImage(original, 0,
-										0, width, height);
-								$(canvas).attr(
-										'title',
-										'Original size: ' + original.width
-												+ 'x' + original.height);
-								return canvas;
 							}
-
-							$(new Image())
-									.on(
-											'load',
-											function(e) {
-												console.log('imgobj', e)
-												var $wrapper = $(
-														'<li name="addImg" class="new-item"><div class="list-content"><span class="preview"></span><span class="type">'
-																+ imgObj.name
-																+ '<br>'
-																+ '<br>'
-																+ '<textarea rows="10pt" cols="60px" style="resize:none;"></textarea></span><span class="imagedelete" title="Remove image"><img class="rcp-delImg" src="img/close.png"></span></div></li>')
-														.appendTo('#output ul');
-												$('.rcp-delImg', $wrapper)
-														.one(
-																'click',
-																function(e) {
-																	$wrapper
-																			.toggleClass(
-																					'new-item')
-																			.addClass(
-																					'removed-item');
-																	$wrapper
-																			.one(
-																					'animationend webkitAnimationEnd MSAnimationEnd oAnimationEnd',
-																					function(
-																							e) {
-																						$wrapper
-																								.remove();
-																					});
-																});
-
-												var thumb = getThumbnail(
-														e.target, 50, 50);
-												var $link = $(
-														'<a rel="fancybox">')
-														.attr(
-																{
-																	target : "_blank",
-																	href : imgObj.imgSrc
-																})
-														.append(thumb)
-														.appendTo(
-																$('.preview',
-																		$wrapper));
-
-											}).attr('src', imgObj.imgSrc);
-
+							canvas.width = width;
+							canvas.height = height;
+							canvas.getContext("2d").drawImage(original, 0, 0,
+									width, height);
+							$(canvas).attr(
+									'title',
+									'Original size: ' + original.width + 'x'
+											+ original.height);
+							return canvas;
 						}
 
-						$("a[rel=fancybox]").fancybox();
+						$(new Image())
+								.on(
+										'load',
+										function(e) {
+											console.log('imgobj', e)
+											var $wrapper = $(
+													'<li name="addImg" class="new-item"><div class="list-content"><span class="preview"></span><span class="type">'
+															+ imgObj.name
+															+ '<br>'
+															+ '<br>'
+															+ '<textarea rows="10pt" cols="60px" style="resize:none;"></textarea></span><span class="imagedelete" title="Remove image"><img class="rcp-delImg" src="img/close.png"></span></div></li>')
+													.appendTo('#output ul');
+											$('.rcp-delImg', $wrapper)
+													.one(
+															'click',
+															function(e) {
+																$wrapper
+																		.toggleClass(
+																				'new-item')
+																		.addClass(
+																				'removed-item');
+																$wrapper
+																		.one(
+																				'animationend webkitAnimationEnd MSAnimationEnd oAnimationEnd',
+																				function(
+																						e) {
+																					$wrapper
+																							.remove();
+																				});
+															});
 
-						var fileReaderAvailable = (typeof FileReader !== "undefined");
-						var clipBoardAvailable = (window.clipboardData !== false);
-						var pasteAvailable = Boolean(clipBoardAvailable
-								& fileReaderAvailable
-								& !eval('/*@cc_on !@*/false'));
+											var thumb = getThumbnail(e.target,
+													50, 50);
+											var $link = $('<a rel="fancybox">')
+													.attr({
+														target : "_blank",
+														href : imgObj.imgSrc
+													}).append(thumb).appendTo(
+															$('.preview',
+																	$wrapper));
 
-						if (fileReaderAvailable) {
+										}).attr('src', imgObj.imgSrc);
 
-							// Enable drop area upload
-							$('#dropzone').imageUpload({
-								errorContainer : $('span', '#errormessages'),
-								trigger : 'dblclick',
-								enableCliboardCapture : pasteAvailable,
-								onBeforeUpload : function() {
-									$('body').css('background-color', 'green');
-									console.log('start', Date.now());
-								},
-								onAfterUpload : function() {
-									$('body').css('background-color', '#eee');
-									console.log('end', Date.now());
-								},
-								outputHandler : outputHandlerFunc
-							})
+					}
 
-							$('#dropzone')
-									.prev('#textbox-wrapper')
-									.find('#textbox')
-									.append(
-											'<p class="large">드래그로<br>업로드</p><p class="small">더블클릭으로<br>업로드하기</p>');
-						} else {
-							$('body').addClass('nofilereader');
-						}
+					$("a[rel=fancybox]").fancybox();
 
-						if (!pasteAvailable) {
-							$('body').addClass('nopaste');
-						}
+					var fileReaderAvailable = (typeof FileReader !== "undefined");
+					var clipBoardAvailable = (window.clipboardData !== false);
+					var pasteAvailable = Boolean(clipBoardAvailable
+							& fileReaderAvailable & !eval('/*@cc_on !@*/false'));
 
-					});
-	
-	$('#appmtbtn')
-	.click(
-			function() {
-				$("#appendSlot")
-						.append('<div name="addSlot">'
-								+'<div class="col-md-4 col-md-offset-3" name="addMt">'
-										+ '<input class="form-control rcp-formdata" type="text" name="materialName"'
-										+'placeholder="재료를 입력해주세요"></div><div class="col-md-1" name="addQt">'
-										+ '<input class="form-control rcp-formdata" type="text" name="materialAmount" placeholder="예)400g"></div>'
-										+'<div class="col-md-1 name="delMt"><button type="button" class="btn btn-default rcp-delMtSlot" id="delMtBtn" name="delMtSlot">삭제</button></div></div>');
-			})
+					if (fileReaderAvailable) {
 
-$(document).on('click',"button[name='delMtSlot']",function() {
+						// Enable drop area upload
+						$('#dropzone').imageUpload({
+							errorContainer : $('span', '#errormessages'),
+							trigger : 'dblclick',
+							enableCliboardCapture : pasteAvailable,
+							onBeforeUpload : function() {
+								$('body').css('background-color', 'green');
+								console.log('start', Date.now());
+							},
+							onAfterUpload : function() {
+								$('body').css('background-color', '#eee');
+								console.log('end', Date.now());
+							},
+							outputHandler : outputHandlerFunc
+						})
+
+						$('#dropzone')
+								.prev('#textbox-wrapper')
+								.find('#textbox')
+								.append(
+										'<p class="large">드래그로<br>업로드</p><p class="small">더블클릭으로<br>업로드하기</p>');
+					} else {
+						$('body').addClass('nofilereader');
+					}
+
+					if (!pasteAvailable) {
+						$('body').addClass('nopaste');
+					}
+
+				});
+
+$('#appmtbtn')
+		.click(
+				function() {
+					$("#appendSlot")
+							.append(
+									'<div name="addSlot">'
+									+'<div class="row form-group form-group-lg form-group-md form-group-sm">'
+									+'<span class="lablebox col-md-7">'
+									+'<input name="materialName" type="text" placeholder="재료명"class="inputbox">'
+									+'<span class="inputname">재료명</span>'
+									+'</span>'
+									+'<span class="lablebox col-md-3">'
+									+'<input name="materialAmount" type="text" placeholder="분량(예:400g)"class="inputbox">'
+									+'<span class="inputname">분량</span>'
+									+'</span>'
+									+'<span class="col-md-2" name="delMt">'
+									+'<button type="button" class="btn btn-default rcp-delMtSlot"id="delMtBtn" name="delMtSlot">삭제</button>'
+									+'</span>'
+									+'</div>'
+									+'</div>');
+				})
+
+$(document).on('click', "button[name='delMtSlot']", function() {
 	$(this).parent().parent().remove();
-	
-	
+
 })
-			
-			
-	
