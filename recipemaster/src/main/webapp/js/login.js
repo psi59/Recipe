7afc2f502f643ba5b8d2666f644f06dd2db09ef6
bitcamp <bitcamp@bitcamp-PC>
2016-08-11 21:06:21 +0900

    login시, Bounce -성준모-

--- recipemaster/src/main/webapp/js/login.js
+++ recipemaster/src/main/webapp/js/login.js
@@ -10,10 +10,11 @@ $("#userLogin").click(
 				},
 				success : function(result) {
 					if (result.status != 'success') {
-						
+						$( "#userPassword" ).effect('bounce');
 						$('#loginInputBox > .warning').remove();
 						$('#loginInputBox').append("<div id='warning' class='warning'>아이디 또는 비밀번호를 다시 확인하세요.</div>");
 						$('#warning').css('color','red').css('font-size', '13px');
+						
 						return;
 					} 
  
