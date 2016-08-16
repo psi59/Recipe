f6599ec5028edcfd5e3c41533884a62d6c4ee8f3
BitCamp <BitCamp@BitCamp-PC>
2016-08-16 16:43:12 +0900

    20160816 - 용이 추가     VisitorController 구성
    	* VisitorController.java
    	* 
    		* add/detail/update/delete/list
    
    	* VisitorController.java ( json )
    	* 
    		* add/detail/update/delete/list
    
    	* VisitorDao.java
    	* VisitorService.java
    	* VisitorServiceImple.java
    	* VisitorDao.xml
    	* 
    		* detail/update/delete/list 쿼리 추가
    		* add 아직 주석처리 (uno/uno2)
    
    	* Visitor.java]
    
    
    community.html / community.css / community.js 수정
    
    버튼이미지 3개 수정

---
+++ recipemaster/src/main/java/com/recipe/controller/json/VisitorController.java
@@ -0,0 +1,91 @@
+package com.recipe.controller.json;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.Gson;
+import com.recipe.domain.Visitor;
+import com.recipe.service.VisitorService;
+
+@Controller
+@RequestMapping("/visitor/")
+public class VisitorController {
+  @Autowired VisitorService visitorService;
+
+  @RequestMapping(path="list", produces="application/json;charset=UTF-8")
+  @ResponseBody
+  public String list(@RequestParam(defaultValue="1")int pageNo, 
+      @RequestParam(defaultValue="10")int pageSize){
+
+    HashMap<String,Object> result = new HashMap<>();
+    try{
+      List<Visitor> list = visitorService.getVisitorList(pageNo, pageSize);
+      result.put("status", "success");
+      result.put("data", list);
+    }catch(Exception e){
+      result.put("status", "failure");
+    }
+    return new Gson().toJson(result);
+  }
+
+  @RequestMapping(path="add", method=RequestMethod.POST, produces="application/json;charset=UTF-8")
+  @ResponseBody
+  public String add(Visitor visitor){    
+    HashMap<String,Object> result = new HashMap<>();
+    try{
+      visitorService.addVisitor(visitor);
+      result.put("status", "success");      
+    }catch(Exception e){
+      result.put("status", "failure");
+    }
+    return new Gson().toJson(result);
+  }
+  
+  @RequestMapping(path="detail", 
+      produces="application/json;charset=UTF-8")
+  @ResponseBody
+  public String detail(int no){        
+    HashMap<String,Object> result = new HashMap<>();
+    try{      
+      result.put("status", "success");
+      result.put("data", visitorService.getVisitor(no));
+    }catch(Exception e){
+      result.put("status", "failure");
+    }
+    return new Gson().toJson(result);
+  }
+
+  @RequestMapping(path="update", method=RequestMethod.POST, produces="application/json;charset=UTF-8")
+  @ResponseBody
+  public String update(Visitor visitor){    
+    HashMap<String,Object> result = new HashMap<>();
+    try{
+      visitorService.updateVisitor(visitor);
+      result.put("status", "success");      
+    }catch(Exception e){
+      result.put("status", "failure");
+    }
+    return new Gson().toJson(result);
+  }
+  
+  @RequestMapping(path="delete", produces="application/json;charset=UTF-8")
+  @ResponseBody
+  public String delete(int no){    
+    HashMap<String,Object> result = new HashMap<>();
+    try{
+      visitorService.deleteVisitor(no);
+      result.put("status", "success");      
+    }catch(Exception e){
+      result.put("status", "failure");
+    }
+    return new Gson().toJson(result);
+  }
+  
+}
