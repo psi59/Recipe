8bacb2ec448a46321a3278d523b8bb2ade3bbcf5
BitCamp-PC\BitCamp <yongyi1587@naver.com>
2016-08-29 16:34:14 +0900

    방명록 등록 삭제 업데이트 메인화면 쉐프카드 css 수정 visitor.java /visitorService,impl,dao수정 index.html 구독하기 버튼 class 추가

--- recipemaster/src/main/java/com/recipe/controller/json/VisitorController.java
+++ recipemaster/src/main/java/com/recipe/controller/json/VisitorController.java
@@ -15,24 +15,30 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.google.gson.Gson;
 import com.recipe.domain.User;
 import com.recipe.domain.Visitor;
+import com.recipe.service.UserService;
 import com.recipe.service.VisitorService;
 
 @Controller
 @RequestMapping("/visitor/")
 public class VisitorController {
+  @Autowired UserService userService;
   @Autowired VisitorService visitorService;
 
   @RequestMapping(path="list", produces="application/json;charset=UTF-8")
   @ResponseBody
   public String list(@RequestParam(defaultValue="1")int pageNo, 
-                      @RequestParam(defaultValue="10")int pageSize){
-
+                      @RequestParam(defaultValue="10")int pageSize,
+                      String email){
+    User user = userService.selectFromEmail(email);
+    
     HashMap<String,Object> result = new HashMap<>();
     try{
-      List<Visitor> list = visitorService.getVisitorList(pageNo, pageSize);
+      List<Visitor> list = visitorService.getVisitorList(user.getUserNo(),pageNo, pageSize);
       result.put("status", "success");
       result.put("data", list);
+      result.put("user", user);
     }catch(Exception e){
+      e.printStackTrace();
       result.put("status", "failure");
     }
     return new Gson().toJson(result);
@@ -40,12 +46,15 @@ public class VisitorController {
 
   @RequestMapping(path="add", method=RequestMethod.POST, produces="application/json;charset=UTF-8")
   @ResponseBody
-  public String add(Visitor visitor,HttpSession session){    
+  public String add(Visitor visitor, int toUser, HttpSession session){    
     HashMap<String,Object> result = new HashMap<>();
     try{
-      visitor.setVisitorUserNo((Integer)session.getAttribute("userNo"));
+      visitor.setLoginUserNo(((User)session.getAttribute("loginUser")).getUserNo());
+      visitor.setVisitorUserNo(toUser);
+      System.out.println("로그인한 유저 NO : "+ visitor);
       visitorService.addVisitor(visitor);
-      result.put("status", "success");      
+      result.put("status", "success");    
+ 
     }catch(Exception e){
       result.put("status", "failure");
     }
