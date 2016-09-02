dba216e1bf042eb2b8c9b325fde29d5eeae3851f
Kojaehyun <sle1392@naver.com>
2016-09-02 16:10:38 +0900

    -고재현 메인에 스크랩기능 추가 상세정보에 좋아요 기능 추가 스크랩 버튼 옆에 좋아요 버튼 추가 Mypage에서 레시피 상세정보 보기 - 고려 메인에서 좋아요 클릭시 숫자도 변경 -고려 댓글 탭으로 갔다가 레시피 정보로 이동 - 고

--- recipemaster/src/main/java/com/recipe/controller/json/RecipeController.java
+++ recipemaster/src/main/java/com/recipe/controller/json/RecipeController.java
@@ -298,8 +298,8 @@ public class RecipeController {
   public String list(@RequestParam int userNo, @RequestParam(defaultValue = "4") int pageSize, int request) {
     HashMap<String, Object> result = new HashMap<>();
     List<Recipe> list = recipeService.getRecipeList(userNo, pageSize, request);
-    for (int i = 0; i < list.size(); i++) {
-    }
+System.out.println("session userNo : "+userNo);
+
     try {
       result.put("status", "success");
       result.put("data", list);
@@ -327,6 +327,7 @@ public class RecipeController {
     recipe.setHits(recipe.getHits() + 1);
     recipeService.updateHits(recipe);
     try {
+      System.out.println("recipe : "+recipe);
       result.put("status", "success");
       result.put("data", recipe);
       result.put("materials", materials);
--- recipemaster/src/main/webapp/js/mypage.js
+++ recipemaster/src/main/webapp/js/mypage.js
@@ -1,6 +1,8 @@
 document.write('<script type"text/javascript" src="js/common.js"></script>')
+document.write('<script type"text/javascript" src="js/recipeDetail.js"></script>')
 
 $(function() {
+	//recipeDetail();
 	var userInfo = getUserInfo();
 
 	
@@ -122,7 +124,7 @@ $(function() {
 	loadMyPage();
 		
 Handlebars.registerHelper('transientStorage', function(options) {
-		  if (this.regiStatus == 1) {
+		  if (this.regiStatus == 0) {
 		    return options.fn(this);
 		  } else {
 		    return options.inverse(this);
@@ -131,7 +133,7 @@ Handlebars.registerHelper('transientStorage', function(options) {
 
 Handlebars.registerHelper('regStatus', function(options) {
 	  if ( userInfo == null || location.href.split('?')[1] != userInfo.email ) {
-		if(this.regiStatus == 1){
+		if(this.regiStatus == 0){
 			return options.fn(this);
 		}		  
 	  }else{
