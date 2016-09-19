b6d98f1ca25a22c462af2248ff6d64d230ce64a4
BitCamp-PC\BitCamp <yongyi1587@naver.com>
2016-09-19 20:29:23 +0900

    index.html 랜덤레시피 수정 rankingpage 완료

--- recipemaster/src/main/webapp/js/rankingpage.js
+++ recipemaster/src/main/webapp/js/rankingpage.js
@@ -1,14 +1,14 @@
 $(function() {
 	loadUsers();
-//	$('.rankingWrapper').bxSlider({
-//	startSlide : 0,
-//	pager : false,
-//	moveSlides : 1,
-//	infiniteLoop : true
-//	});
+	$('.rankingWrapper').bxSlider({
+	startSlide : 0,
+	pager : false,
+	moveSlides : 1,
+	infiniteLoop : true
+	});
 	loadMonthRank();
 	loadTodayRank();
-
+	
 	function loadUsers() {
 		var source = $('#main-chefRanking').text();
 		var template = Handlebars.compile(source);
@@ -23,8 +23,6 @@ $(function() {
 					swal('chefCard.js 오류');
 					return;
 				}
-				console.log("result : " +result)
-				console.log("result.data : " +result.data)
 				for (var i = 0; i < result.data.length; i++) {
 					if (result.data[i].subscribeUser == 0) {
 						result.data[i].status = null;
@@ -98,12 +96,93 @@ $(function() {
 			}
 		});
 	}// end of 쉐프카드
+	
+    var sourceChef = $('#chef-rank-template').text();
+    var templateChef = Handlebars.compile(sourceChef);
+    loadChefRanking();
+
+    function loadChefRanking() {
+      $.ajax({
+        url : 'user/rank.json',
+        dataType : 'json',
+        method : 'get',
+        success : function(result) {
+          if (result.status != 'success') {
+            alert('실행중 오류 발생');
+            return;
+          }
+          var list = result.data;
+          $('#chefRanking').append(templateChef(result));
+        },
+        error : function() {
+          alert('서버 요청 오류');
+        }
+      });
+    }
+    
+    var sourceRcp = $('#recipe-rank-template').text();
+    var templateRcp = Handlebars.compile(sourceRcp);
+    loadRcpRanking();
+
+    function loadRcpRanking() {
+      $.ajax({
+        url : 'recipe/rank.json',
+        dataType : 'json',
+        method : 'get',
+        success : function(result) {
+          if (result.status != 'success') {
+             alert('실행중 오류 발생');
+            return;
+          }
+          var list = result.data;
+          $('#recipeRanking').append(templateRcp(result));
+        },
+        error : function() {
+           alert('서버 요청 오류');
+        }
+      });
+    }
+    
+    var sourceMy = $('#chef-myrank-template').text();
+    var template1 = Handlebars.compile(sourceMy);
+    loadMyRanking();
+
+    function loadMyRanking() {
+      $.ajax({
+        url : 'user/myrank.json',
+        dataType : 'json',
+        method : 'get',
+        success : function(result) {
+          if (result.status != 'success') {
+        	  alert('실패');
+            return;
+          }
+          var list = result.data;
+          if(list!=null){
+        	  var appendData = 
+        		  '<h2 class="rcp-ranking-middle-background-comment" id="chefMyRanking">'
+        		  +'내 순위는 ' +list.rownum+'등 입니다. [ '+list.grade+' ] <br/>'+
+        		  '레시피 '+list.recipeCount+'개 / 구독자 '+list.subsCount+'명 / 토탈포인트 '
+        		  +list.totalPoint+'점</h2>'
+        		    
+                  $('.rcp-profileCover').append($(appendData));
+          }
+          
+          console.log(result);
+        },
+        error : function() {
+          alert('서버 요청 오류');
+        }
+      });
+    }
+    
+    
 });
 
 $(document).on('click', '.rcp-userName, .rcp-nickname , .rcp-profile',function(event){
 	event.preventDefault();
 	console.log( "event target : "+$(event.target).attr('class') )
-	$(location).attr('href',contextRoot+'/mypage.html?'+$(event.target).parent().children('input[type="hidden"]').val() );
+	$(location).attr('href',contextRoot+'mypage.html?'+$(event.target).parent().children('input[type="hidden"]').val() );
 	console.log("email val()"+$(event.target).parent().children('input[name="email"]').val() );
 })
 
